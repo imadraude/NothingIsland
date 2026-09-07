e9eb22dbfb4b8208b992e51bc6e144411c3000d8
imadraude <imadraude777@gmail.com>
2026-09-07 04:01:19 +0300

    chore(release): bump version to v0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = gitCommitCount.get()
-        versionName = "0.1.4"
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
